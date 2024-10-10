2e4b3471ae2ff2b169236a89486aa99829b5509a
Kaito1290 <sirobu9963@gmail.com>
2024-10-10 12:16:15 +0900

    feat: Storybookの環境構築をする

--- src/components/exampleTest/example.tsx
+++ src/components/exampleTest/example.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import styles from "./example.module.css";
 
-export default function Home() {
+type Props = {
+  text: string;
+};
+export default function Home({ text }: Props) {
   return (
     <div>
       <h1 className={styles.h1}>Home</h1>
       <Link href="/about">About</Link>
+      <p>{text}</p>
     </div>
   );
 }
