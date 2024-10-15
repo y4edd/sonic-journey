66a39191569b5fdd372b5907e8b78790d6b7e6b5
shotaniguchi <sho.taniguchi@rakus-partners.co.jp>
2024-10-15 23:57:18 +0900

    feat: ヘッダー部にあいまい検索機能追加。レイアウトの違いの為、freeSearchコンポーネントは使用していません。

--- src/components/frame/header/header.tsx
+++ src/components/frame/header/header.tsx
@@ -5,34 +5,67 @@ import styles from "./header.module.css";
 import SearchIcon from "@mui/icons-material/Search";
 import { HamburgerMenu } from "./hamburgerMenu";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { PageHeadingList } from "./pageHeadingList";
+import { useState } from "react";
+import { useFreeWordSearch } from "@/hooks/top/useFreeWordSearch";
 
 const Header = () => {
-  const pathname = usePathname();
-  console.log(pathname, "pth");
+  const [searchOn, setSearchOn] = useState(false);
+  const { handleForm, handleChange, error, freeWord } = useFreeWordSearch();
+  const handleClickSearch = () => {
+    setSearchOn((prevState) => !prevState);
+  };
+
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.headerTop}>
-          <HamburgerMenu />
-          <div className={styles.logoImage}>
-            <Link href="/">
-              <Image
-                src="/images/yaetunes_logo_transparent.png"
-                alt="header-logo"
-                height={40}
-                width={160}
-              />
-            </Link>
-          </div>
-          <div className={styles.searchIcon}>
-            <SearchIcon className={styles.searchIconMark} />
-          </div>
+        <div>
+          {searchOn ? (
+            <div className={styles.headerTop}>
+              <form onSubmit={handleForm} className={styles.freeSearch}>
+                <input
+                  type="text"
+                  name="freeWord"
+                  value={freeWord}
+                  onChange={handleChange}
+                  placeholder={
+                    error ? error : "アーティスト・アルバム・楽曲で検索"
+                  }
+                />
+                <button type="submit">
+                  <SearchIcon />
+                </button>
+              </form>
+              <div className={styles.searchCancel}>
+                <p
+                  className={styles.searchCancelText}
+                  onClick={handleClickSearch}
+                >
+                  キャンセル
+                </p>
+              </div>
+            </div>
+          ) : (
+            <div className={styles.headerTop}>
+              <HamburgerMenu />
+              <div className={styles.logoImage}>
+                <Link href="/">
+                  <Image
+                    src="/images/yaetunes_logo_transparent.png"
+                    alt="header-logo"
+                    height={40}
+                    width={160}
+                  />
+                </Link>
+              </div>
+              <div className={styles.searchIcon} onClick={handleClickSearch}>
+                <SearchIcon className={styles.searchIconMark} />
+              </div>
+            </div>
+          )}
         </div>
         <hr />
-        <ul className={styles.headerBottom}>
-          <li></li>
-        </ul>
+        <PageHeadingList />
         <hr />
       </div>
     </>
