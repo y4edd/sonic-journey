2248fdfca3dfe1651ba5c2a2ea5b99d5f0306af0
fukuda0801 <nagisano0801@gmail.com>
2024-10-15 15:36:32 +0900

    feat: トップページ人気新着楽曲表示

--- src/app/(top)/page.tsx
+++ src/app/(top)/page.tsx
@@ -1,6 +1,7 @@
 import ContentTitle from "@/components/top/ContentTitle/ContentTitle";
 import FreeSearch from "@/components/top/FreeSearch/FreeSearch";
 import Slider from "@/components/top/Slider/Slider";
+import SongsGroup from "@/components/top/SongsGroup/SongsGroup";
 import styles from "./page.module.css";
 
 const TopPage = () => {
@@ -11,8 +12,14 @@ const TopPage = () => {
           <ContentTitle title="特集" />
           <Slider />
         </div>
+
         <div className={styles.freeSearchContent}>
           <FreeSearch />
+        </div>
+
+        <div className={styles.newSongsContent}>
+          <ContentTitle title="人気新着" />
+          <SongsGroup />
         </div>
       </div>
     </main>
