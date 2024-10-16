addbb5e1c0954da65aa159b4ee92a43e0d89d25c
fukuda0801 <nagisano0801@gmail.com>
2024-10-16 15:04:35 +0900

    feat: ジャンルごとのアーティスト情報を取得できる関数を作成

--- src/utils/apiFunc.ts
+++ src/utils/apiFunc.ts
@@ -1,4 +1,5 @@
 // 人気新着楽曲を取得する関数
+// limitには取得したい件数を入力
 export const getNewSongs = async (limit: number) => {
   try {
     const res = await fetch(`http://localhost:3000/api/newSongsSearch?limit=${limit}`);
@@ -14,9 +15,26 @@ export const getNewSongs = async (limit: number) => {
 };
 
 // シングルランキング楽曲を取得する関数
+// limitには取得したい件数を入力
 export const getRankSingleSongs = async (limit: number) => {
   try {
     const res = await fetch(`http://localhost:3000/api/rankSingleSongSearch?limit=${limit}`);
+
+    if (!res.ok) {
+      throw new Error("データが見つかりませんでした");
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// ジャンルごとのアーティスト情報を取得する関数
+// genreにはgenreのid
+export const getGenreArtist = async (genre: number) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/genreArtistSearch?genre=${genre}`);
 
     if (!res.ok) {
       throw new Error("データが見つかりませんでした");
