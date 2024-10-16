80941eaab8cba58d9fdacdaf69e591d6632507ac
fukuda0801 <nagisano0801@gmail.com>
2024-10-16 10:46:46 +0900

    feat: シングルランキング、人気新作楽曲ともに画像を使用できるように変更

--- src/components/top/SongContent/SongContent.tsx
+++ src/components/top/SongContent/SongContent.tsx
@@ -1,12 +1,17 @@
-import type { DeezerNewSong, DeezerNewSongDetail } from "@/types/deezer";
+import type { DeezerSong } from "@/types/deezer";
 import Image from "next/image";
 import styles from "./SongContent.module.css";
 
-const SongContent = ({ song }: { song: DeezerNewSong }) => {
+const SongContent = ({ song }: { song: DeezerSong }) => {
   return (
     // FIXME: 後でリンクにする必要があります。
     <div className={styles.songInfo}>
-      <Image src={song.cover_xl} alt={`${song.title}のジャケット画像`} width={180} height={180} />
+      <Image
+        src={song.cover_xl ?? song.album.cover_xl}
+        alt={`${song.title}のジャケット画像`}
+        width={180}
+        height={180}
+      />
       <p>{song.artist.name}</p>
       <p>{song.title}</p>
     </div>
