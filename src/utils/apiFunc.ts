7c0c5ea9e0b65abaf90b318d541ff6e7250982f2
fukuda0801 <nagisano0801@gmail.com>
2024-10-16 11:43:32 +0900

    fix: 楽曲データ取得関数にエラーハンドリングを追加

--- src/utils/apiFunc.ts
+++ src/utils/apiFunc.ts
@@ -1,11 +1,29 @@
 // 人気新着楽曲を取得する関数
 export const getNewSongs = async (limit: number) => {
-  const res = await fetch(`http://localhost:3000/api/newSongsSearch?limit=${limit}`);
-  return await res.json();
+  try {
+    const res = await fetch(`http://localhost:3000/api/newSongsSearch?limit=${limit}`);
+
+    if (!res.ok) {
+      throw new Error("データが見つかりませんでした");
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // シングルランキング楽曲を取得する関数
 export const getRankSingleSongs = async (limit: number) => {
-  const res = await fetch(`http://localhost:3000/api/rankSingleSongSearch?limit=${limit}`);
-  return await res.json();
+  try {
+    const res = await fetch(`http://localhost:3000/api/rankSingleSongSearch?limit=${limit}`);
+
+    if (!res.ok) {
+      throw new Error("データが見つかりませんでした");
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
 };
