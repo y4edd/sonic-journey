d3d45a0454d98d36272059e5f3b985fca95808fc
y4edd <yaeyuu0116@icloud.com>
2024-10-17 17:39:55 +0900

    fix:インデント修正

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,41 +1,43 @@
 "use client";
-import { useForm, SubmitHandler } from "react-hook-form";
-import "./page.css";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import ContentTitle from "@/components/top/ContentTitle/ContentTitle";
-import { error } from "console";
+import { type SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { ErrorSharp } from "@mui/icons-material";
-import { RequestCookiesAdapter } from "next/dist/server/web/spec-extension/adapters/request-cookies";
-import Link from "next/link";
+import "./page.css";
 
 // Zod のスキーマを定義
-const schema = z.object({
-  name: z.string().min(1, "ユーザー名は必須です").max(20,"20文字以内です"),
-  mailAddress: z.string().email("正しいメールアドレスを入力してください"),
-  password: z.string().min(6, "パスワードは6文字以上で入力してください"),
-  passwordConfirm: z.string().min(6, "パスワード確認は6文字以上で入力してください"),
-}).refine((data) => data.password === data.passwordConfirm, {
-  message: "パスワードが一致しません",
-  path: ["passwordConfirm"],
-});
+const schema = z
+  .object({
+    name: z.string().min(1, "ユーザー名は必須です").max(20, "20文字以内です"),
+    mailAddress: z.string().email("正しいメールアドレスを入力してください"),
+    password: z.string().min(6, "パスワードは6文字以上で入力してください"),
+    passwordConfirm: z.string().min(6, "パスワード確認は6文字以上で入力してください"),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "パスワードが一致しません",
+    path: ["passwordConfirm"],
+  });
 
 // zodの型定義
 type FormData = z.infer<typeof schema>;
 
 const UserRegistration = () => {
   // useStateでサーバーエラー管理
-  const [_serverError,_setServerError] = useState<string | null>(null);
+  const [_serverError, _setServerError] = useState<string | null>(null);
   // Reacr hook formでフォーム管理
-  const { register, handleSubmit,formState:{errors} } = useForm<FormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
 
   const router = useRouter();
 
-  const onSubmit:SubmitHandler<FormData> = async(data: FormData) => {
+  const onSubmit: SubmitHandler<FormData> = async (data: FormData) => {
     // try {
     //   await registerUser(
     //     data.name,
@@ -56,9 +58,10 @@ const UserRegistration = () => {
     <>
       <div>アカウント登録</div>
       <div className="main">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-register">
-              <label>ユーザー名</label>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-register">
+            <label>
+              ユーザー名
               <input
                 type="text"
                 {...register("name", {
@@ -68,10 +71,12 @@ const UserRegistration = () => {
                 id="userName"
                 placeholder="tanitune"
               />
-              {errors.name && <span className="error-message">{String(errors.name.message)}</span>}
-            </div>
-            <div className="form-register">
-              <label>メールアドレス</label>
+            </label>
+            {errors.name && <span className="error-message">{String(errors.name.message)}</span>}
+          </div>
+          <div className="form-register">
+            <label>
+              メールアドレス
               <input
                 type="email"
                 {...register("mailAddress", {
@@ -81,10 +86,14 @@ const UserRegistration = () => {
                 id="mailAddress"
                 placeholder="tanisan@example.com"
               />
-              {errors.mailAddress && <span className="error-message">{String(errors.mailAddress.message)}</span>}
-            </div>
-            <div className="form-register">
-              <label>パスワード入力</label>
+            </label>
+            {errors.mailAddress && (
+              <span className="error-message">{String(errors.mailAddress.message)}</span>
+            )}
+          </div>
+          <div className="form-register">
+            <label>
+              パスワード入力
               <input
                 type="password"
                 {...register("password", {
@@ -94,10 +103,14 @@ const UserRegistration = () => {
                 id="password"
                 placeholder="password"
               />
-              {errors.password && <span className="error-message">{String(errors.password.message)}</span>}
-            </div>
-            <div className="form-register">
-              <label>パスワード確認</label>
+            </label>
+            {errors.password && (
+              <span className="error-message">{String(errors.password.message)}</span>
+            )}
+          </div>
+          <div className="form-register">
+            <label>
+              パスワード確認
               <input
                 type="password"
                 {...register("passwordConfirm", {
@@ -107,14 +120,19 @@ const UserRegistration = () => {
                 id="passwordConfirm"
                 placeholder="password"
               />
-              {errors.passwordConfirm && <span className="error-message">{String(errors.passwordConfirm.message)}</span>}
-            </div>
-            <input type="submit" value="ユーザー登録" className="register" />
-          </form>
+            </label>
+            {errors.passwordConfirm && (
+              <span className="error-message">{String(errors.passwordConfirm.message)}</span>
+            )}
+          </div>
+          <input type="submit" value="ユーザー登録" className="register" />
+        </form>
       </div>
-      <p>登録済みの方は<Link href="/login">ログイン</Link></p>
+      <p>
+        登録済みの方は<Link href="/login">ログイン</Link>
+      </p>
     </>
-  )
+  );
 };
 
 export default UserRegistration;
