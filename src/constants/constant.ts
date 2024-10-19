518a40a799cc1cba9d46aed5a055a40a31b0e4ee
shotaniguchi <sho.taniguchi@rakus-partners.co.jp>
2024-10-19 13:46:00 +0900

    feat: 月曜日の日付を取得する変数をconstantsディレクトリに配置

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -18,3 +18,21 @@ export const GENRE_ARTISTS = [
   { id: 95, name: "キッズ" },
   { id: 173, name: "映画/ゲーム" },
 ];
+
+const getMondayOfThisWeek = () => {
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const diff = dayOfWeek === 0 ? -6 : 1 - dayOfWeek;
+  const monday = new Date(today.setDate(today.getDate() + diff));
+  return monday;
+};
+export const GETMONDAYOFTHISWEEK = getMondayOfThisWeek();
+
+const getMondayOfLastWeek = () => {
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const diff = dayOfWeek === 0 ? -6 : 1 - dayOfWeek;
+  const monday = new Date(today.setDate(today.getDate() + diff));
+  return monday;
+};
+export const GETMONDAYOFLASTWEEK = getMondayOfLastWeek();
