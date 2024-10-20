5534dc9f2404776c2bade94433cb737d138e69dc
shotaniguchi <sho.taniguchi@rakus-partners.co.jp>
2024-10-20 16:17:23 +0900

    fix: 先週の日時を7日前に修正

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -32,7 +32,7 @@ const getMondayOfLastWeek = () => {
   const today = new Date();
   const dayOfWeek = today.getDay();
   const diff = dayOfWeek === 0 ? -6 : 1 - dayOfWeek;
-  const monday = new Date(today.setDate(today.getDate() + diff));
+  const monday = new Date(today.setDate(today.getDate() + diff - 7));
   return monday;
 };
 export const GETMONDAYOFLASTWEEK = getMondayOfLastWeek();
