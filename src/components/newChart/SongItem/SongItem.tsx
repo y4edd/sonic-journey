92e8f3c7d12972cf8a3ba289da4f064f64e9b830
shotaniguchi <sho.taniguchi@rakus-partners.co.jp>
2024-10-21 10:35:18 +0900

    test: biomeのチェックの下修正

--- src/components/newChart/SongItem/SongItem.tsx
+++ src/components/newChart/SongItem/SongItem.tsx
@@ -18,14 +18,11 @@ export const SongItem = ({
     if (weekCheck === "this") {
       songs = songs.filter(
         (song) =>
-          GETMONDAYOFTHISWEEK.toLocaleDateString().replaceAll("/", "-") <=
-          song.release_date
+          GETMONDAYOFTHISWEEK.toLocaleDateString().replaceAll("/", "-") <= song.release_date,
       );
     } else if (weekCheck === "last") {
       songs = songs.filter(
-        (song) =>
-          GETMONDAYOFTHISWEEK.toLocaleDateString().replaceAll("/", "-") >
-          song.release_date
+        (song) => GETMONDAYOFTHISWEEK.toLocaleDateString().replaceAll("/", "-") > song.release_date,
       );
     } else {
     }
@@ -45,9 +42,7 @@ export const SongItem = ({
                 className={styles.songImageGrid}
               />
               <p className={styles.songNameGrid}>
-                {song.title.length <= 15
-                  ? song.title
-                  : `${song.title.slice(0, 14)}...`}
+                {song.title.length <= 15 ? song.title : `${song.title.slice(0, 14)}...`}
               </p>
               <p className={styles.artistNameGrid}>
                 {song.artist.name.length <= 15
