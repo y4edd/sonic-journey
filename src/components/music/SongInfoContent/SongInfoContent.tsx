f465595200cfd64e8e92385bbd01f59f91234d2b
fukuda0801 <nagisano0801@gmail.com>
2024-10-21 09:46:33 +0900

    feat: お気に入り追加ボタン作成、処理未実装

--- src/components/music/SongInfoContent/SongInfoContent.tsx
+++ src/components/music/SongInfoContent/SongInfoContent.tsx
@@ -2,6 +2,7 @@ import CreateNewFolderIcon from "@mui/icons-material/CreateNewFolder";
 import Image from "next/image";
 import SongAudio from "../SongAudio/SongAudio";
 import styles from "./SongInfoContent.module.css";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 type SongInfoContentProps = {
   title: string;
@@ -10,11 +11,22 @@ type SongInfoContentProps = {
   preview?: string;
 };
 
-const SongInfoContent = ({ title, artist, image, preview }: SongInfoContentProps) => {
+const SongInfoContent = ({
+  title,
+  artist,
+  image,
+  preview,
+}: SongInfoContentProps) => {
   return (
     <div>
       <div className={styles.songInfoContent}>
-        <Image src={image} alt={`${title}のジャケット`} width={130} height={130} priority />
+        <Image
+          src={image}
+          alt={`${title}のジャケット`}
+          width={130}
+          height={130}
+          priority
+        />
         <div className={styles.songInfoDetail}>
           <h2>{title}</h2>
           <p>{artist}</p>
@@ -22,6 +34,10 @@ const SongInfoContent = ({ title, artist, image, preview }: SongInfoContentProps
             <SongAudio preview={preview} />
           </div>
           {/* FIXME: プレイリストに追加する処理を記述する必要があります。 */}
+          <div className={styles.songInfoAddFavorite}>
+            <FavoriteBorderIcon />
+            <p>お気に入りに追加</p>
+          </div>
           <div className={styles.songInfoAddList}>
             <CreateNewFolderIcon />
             <p>プレイリストに追加</p>
