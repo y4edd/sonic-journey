026d710b96f4f2f009c857eb39f4320f772b5b3e
fukuda0801 <nagisano0801@gmail.com>
2024-10-22 18:07:34 +0900

    feat: emptyページ内をコンポーネントに分割

--- src/app/music/0/page.tsx
+++ src/app/music/0/page.tsx
@@ -1,28 +1,7 @@
-import StarIcon from "@mui/icons-material/Star";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "../../unexpected.module.css";
+import EmptyContent from "@/components/error/EmptyContent/EmptyContent";
 
-const NotFoundPage = () => {
-  return (
-    <div className={styles.unexpectedPage}>
-      <div className={styles.unexpectedContent}>
-        <h1>
-          <StarIcon sx={{ color: "#fff834" }} />
-          大当たり
-          <StarIcon sx={{ color: "#fff834" }} />
-        </h1>
-        <p>こちらの楽曲情報は存在しません。</p>
-        <p>
-          このページを訪れたあなたはとても幸運です。
-          <br />
-          宝くじを買いに行きましょう！！
-        </p>
-        <Image src="/images/cat.jpg" alt="not-found画像" width={300} height={200} priority />
-        <Link href="/">トップページへ戻る</Link>
-      </div>
-    </div>
-  );
+const EmptyMusicPage = () => {
+  return <EmptyContent title="楽曲" />;
 };
 
-export default NotFoundPage;
+export default EmptyMusicPage;
