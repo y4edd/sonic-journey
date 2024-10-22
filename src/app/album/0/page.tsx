43046507135b5d65281eb938b5ce4c06678c120b
fukuda0801 <nagisano0801@gmail.com>
2024-10-22 16:55:45 +0900

    feat: topページにて表示された、デフォルトの人気新着アルバムがクリックされた場合に表示されるページを作成

---
+++ src/app/album/0/page.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../../unexpected.module.css";
+import StarIcon from "@mui/icons-material/Star";
+
+const NotFoundPage = () => {
+  return (
+    <div className={styles.unexpectedPage}>
+      <div className={styles.unexpectedContent}>
+        <h1>
+          <StarIcon sx={{ color: "#fff834" }} />
+          大当たり
+          <StarIcon sx={{ color: "#fff834" }} />
+        </h1>
+        <p>こちらのアルバム情報は存在しません。</p>
+        <p>
+          このページを訪れたあなたはとても幸運です。
+          <br />
+          宝くじを買いに行きましょう！！
+        </p>
+        <Image
+          src="/images/cat.jpg"
+          alt="not-found画像"
+          width={300}
+          height={200}
+          priority
+        />
+        <Link href="/">トップページへ戻る</Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
