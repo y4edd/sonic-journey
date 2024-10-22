af8268058ffa522a89659ec463deee5d99acbdc9
Kaito1290 <sirobu9963@gmail.com>
2024-10-22 10:25:36 +0900

    style: muiのアイコンとcssを修正

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,11 +1,11 @@
 import MenuBox from "@/components/mypage/MenuBox/MenuBox";
 import BreadList from "@/components/top/BreadList/BreadList";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import HistoryIcon from "@mui/icons-material/History";
 import LogoutIcon from "@mui/icons-material/Logout";
 import MusicNoteTwoToneIcon from "@mui/icons-material/MusicNoteTwoTone";
 import NoAccountsIcon from "@mui/icons-material/NoAccounts";
 import PeopleOutlineIcon from "@mui/icons-material/PeopleOutline";
-import PlayArrowTwoToneIcon from "@mui/icons-material/PlayArrowTwoTone";
 import PlaylistPlayTwoToneIcon from "@mui/icons-material/PlaylistPlayTwoTone";
 import styles from "./page.module.css";
 
@@ -26,17 +26,24 @@ const MyPage = () => {
           mainTitle="お気に入り"
           subTitle="楽曲"
           icon={<MusicNoteTwoToneIcon fontSize="large" />}
+          link="/mypage/favoritesong"
         />
         <MenuBox
           mainTitle="お気に入り"
           subTitle="アーティスト"
           icon={<PeopleOutlineIcon fontSize="large" />}
+          link="/mypage/favoriteartist"
         />
-        <MenuBox mainTitle="プレイリスト" icon={<PlaylistPlayTwoToneIcon fontSize="large" />} />
+        <MenuBox
+          mainTitle="プレイリスト"
+          icon={<PlaylistPlayTwoToneIcon fontSize="large" />}
+          link="/mypage/playlist"
+        />
         <MenuBox
           mainTitle="再生履歴"
           subTitle="（最新10件）"
-          icon={<PlayArrowTwoToneIcon fontSize="large" />}
+          icon={<HistoryIcon fontSize="large" />}
+          link="/mypage/history"
         />
       </div>
       <div className={styles.menuTitle}>
@@ -47,10 +54,19 @@ const MyPage = () => {
           mainTitle="ユーザー情報"
           subTitle="編集・確認"
           icon={<AccountBoxIcon fontSize="large" />}
+          link="/user/edit"
         />
-        <MenuBox mainTitle="ログアウト" icon={<LogoutIcon fontSize="large" />} />
+        <MenuBox
+          mainTitle="ログアウト"
+          icon={<LogoutIcon fontSize="large" />}
+          link="/user/logout"
+        />
         <div className={styles.gridRow}>
-          <MenuBox mainTitle="退会" icon={<NoAccountsIcon fontSize="large" />} />
+          <MenuBox
+            mainTitle="退会"
+            icon={<NoAccountsIcon fontSize="large" />}
+            link="/user/delete"
+          />
         </div>
       </div>
     </div>
