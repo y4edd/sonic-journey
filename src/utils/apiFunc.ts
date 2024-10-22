dbb5d342bf1ce62ca5a7875fdae10d630852bb6e
fukuda0801 <nagisano0801@gmail.com>
2024-10-22 16:52:02 +0900

    fix: api からの取得データが要求した件数を満たさなかった場合にデフォルトの値が入るよう修正

--- src/utils/apiFunc.ts
+++ src/utils/apiFunc.ts
@@ -18,13 +18,13 @@ export const getNewSongs = async (limit: number) => {
     if (result.resultData.length < limit && limit === 4) {
       for (let i = 0; i < limit - result.resultData.length; i++) {
         result.resultData.push({
-          id: 1,
-          title: "BONBON GiRL",
+          id: 0,
+          title: "title",
           cover_xl: "/images/defaultsong.png",
-          release_date: "2024-10-22",
+          release_date: "20xx-xx-xx",
           artist: {
             id: 1,
-            name: "SARM",
+            name: "artist",
           },
         });
       }
@@ -50,7 +50,25 @@ export const getRankSingleSongs = async (limit: number) => {
       throw new Error("データが見つかりませんでした");
     }
 
-    return await res.json();
+    const result = await res.json();
+
+    if (result.resultData.length < limit && limit === 4) {
+      result.resultData.push({
+        id: 0,
+        title: "title",
+        artist: {
+          id: 1,
+          name: "artist",
+        },
+        album: {
+          id: 1,
+          title: "album",
+          cover_xl: "/images/defaultsong.png",
+        },
+      });
+    }
+
+    return result;
   } catch (error) {
     console.error(error);
   }
