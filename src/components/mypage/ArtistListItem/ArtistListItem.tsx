1cc113f3ce23d0bf28bc41b4b9aab636750c1567
Kaito1290 <sirobu9963@gmail.com>
2024-10-22 11:38:03 +0900

    ArtistListItemコンポーネントからアーティスト詳細ページに遷移できるようにする

--- src/components/mypage/ArtistListItem/ArtistListItem.tsx
+++ src/components/mypage/ArtistListItem/ArtistListItem.tsx
@@ -1,23 +1,26 @@
 import type { DeezerArtist } from "@/types/deezer";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./ArtistListItem.module.css";
 
 const ArtistListItem = ({ artist }: { artist: DeezerArtist }) => {
   return (
-    <div className={styles.artistInfo}>
-      <Image
-        src={artist.picture_xl || ""}
-        alt={`${artist.name}のアーティスト画像`}
-        width={75}
-        height={75}
-        priority
-      />
-      <div className={styles.artistName}>
-        <p>{artist.name}</p>
+    <Link href={`/artist/${artist.id}`}>
+      <div className={styles.artistInfo}>
+        <Image
+          src={artist.picture_xl || ""}
+          alt={`${artist.name}のアーティスト画像`}
+          width={75}
+          height={75}
+          priority
+        />
+        <div className={styles.artistName}>
+          <p>{artist.name}</p>
+        </div>
+        <ArrowForwardIosIcon fontSize="small" color="disabled" />
       </div>
-      <ArrowForwardIosIcon fontSize="small" color="disabled" />
-    </div>
+    </Link>
   );
 };
 
