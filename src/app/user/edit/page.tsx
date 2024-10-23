2c8f9220f99ec9e9d9ecaaf4407d3e854f1270e8
y4edd <yaeyuu0116@icloud.com>
2024-10-23 10:26:29 +0900

    fix: バリデーションメッセージの修正

---
+++ src/app/user/edit/page.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import BreadList from "@/components/top/BreadList/BreadList";
+import Button from "@/components/user/Button/Button";
+import ButtonStyles from "@/components/user/Button/Button.module.css";
+import FormInput from "@/components/user/Form/FormInput";
+import Information from "@/components/user/Information/Information";
+import { schema } from "@/lib/validation";
+import type { FormData } from "@/types/user";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { type SubmitHandler, useForm } from "react-hook-form";
+import styles from "./page.module.css";
+
+const Edit = () => {
+  // useStateでサーバーエラー管理
+  const [_serverError, _setServerError] = useState<string | null>(null);
+  // React hook formでフォーム管理
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const router = useRouter();
+
+  // FIXME: dataを受け取りますが今は引数無し
+  const onSubmit: SubmitHandler<FormData> = async () => {
+    // try {
+    //   await registerUser(
+    //     data.name,
+    //     data.email,
+    //     data.password,
+    //     data.confirmPassword
+    //   );
+    //   alert("アカウント情報変更完了");
+    //   router.push("/user/login");
+    // } catch (err:any){
+    //   setServerError(err.message || "サーバーエラーです");
+    // }
+    alert("アカウント情報が変更されました");
+    router.push("/mypage");
+  };
+
+  const handleClick = async () => {
+    router.push("user/information");
+  };
+
+  return (
+    <>
+      <BreadList
+        bread={[
+          { link: "/", title: "TOP" },
+          { link: "/user/info", title: "アカウント情報" },
+          { link: "/user/edit", title: "アカウント編集" }
+        ]}
+      />
+      <div>
+        <Information text="アカウント編集" />
+      </div>
+      <div className={styles.container}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormInput
+            label="ユーザー名"
+            id="userName"
+            type="text"
+            name="userName"
+            placeholder="tanitune"
+            register={register}
+            error={errors.userName}
+          />
+          <FormInput
+            label="メールアドレス"
+            id="mailAddress"
+            type="email"
+            name="mailAddress"
+            placeholder="tani@example.com"
+            register={register}
+            error={errors.mailAddress}
+          />
+          <FormInput
+            label="パスワード"
+            id="password"
+            type="password"
+            name="password"
+            placeholder="password"
+            register={register}
+            error={errors.password}
+          />
+          <FormInput
+            label="パスワード確認"
+            id="passwordConfirm"
+            type="password"
+            name="passwordConfirm"
+            placeholder="password"
+            register={register}
+            error={errors.passwordConfirm}
+          />
+          <Button type="submit" className={ButtonStyles.register} text={"更新"} />
+          <Button type="button" className={ButtonStyles.return} text={"戻る"} onClick={handleClick} />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Edit;
--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -6,12 +6,12 @@ export const schema = z
     mailAddress: z.string().email("正しいメールアドレスを入力してください"),
     password: z
       .string()
-      .min(6, "6文字以上で入力してください")
-      .max(20, "20文字以内で入力してください"),
+      .min(6, "パスワードは6文字以上で入力してください")
+      .max(20, "パスワードは20文字以内で入力してください"),
     passwordConfirm: z
       .string()
-      .min(6, "6文字以上で入力してください")
-      .max(20, "20文字以内で入力してください"),
+      .min(6, "確認用パスワードは6文字以上で入力してください")
+      .max(20, "確認用パスワードは20文字以内で入力してください"),
   })
   .refine((data) => data.password === data.passwordConfirm, {
     message: "パスワードが一致しません",
