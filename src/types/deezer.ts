bcb7439695de380dd97c3c580df819ca35500a88
fukuda0801 <nagisano0801@gmail.com>
2024-10-25 02:50:11 +0900

    feat: アーティストのアルバム情報を取得するAPIにて使用する型を追加

--- src/types/deezer.ts
+++ src/types/deezer.ts
@@ -220,3 +220,34 @@ export type AlbumSingle = {
   preview: string;
   cover_xl: string;
 };
+
+// アーティストのアルバム情報を取得するapiで使用する型
+export type ArtistAlbum = {
+  id: number;
+  title: string;
+  link: string;
+  cover: string;
+  cover_small: string;
+  cover_medium: string;
+  cover_big: string;
+  cover_xl: string;
+  md5_image: string;
+  genre_id: number;
+  nb_tracks: number;
+  record_type: string;
+  tracklist: string;
+  explicit_lyrics: boolean;
+  artist: {
+    id: number;
+    name: string;
+    link: string;
+    picture: string;
+    picture_small: string;
+    picture_medium: string;
+    picture_big: string;
+    picture_xl: string;
+    tracklist: string;
+    type: string;
+  };
+  type: string;
+};
