f1e30c92f893388ea5cd61372585a8e52f81be0b
shotaniguchi <sho.taniguchi@rakus-partners.co.jp>
2024-10-24 12:08:42 +0900

    refuctor: song_api_id(bigint)の扱いが難しいためstringに型を変更

---
+++ prisma/migrations/20241024021859_init/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "Favorite_Song" ALTER COLUMN "api_song_id" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "History" ALTER COLUMN "api_song_id" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Pick_Song" ALTER COLUMN "api_song_id" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Playlist_Song" ALTER COLUMN "api_song_id" SET DATA TYPE TEXT;
