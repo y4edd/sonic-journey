106bf2e3e6bf80db671221c5fc9232ebd9888dcf
y4edd <yaeyuu0116@icloud.com>
2024-10-24 14:11:49 +0900

    fix: インデント修正

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -6,25 +6,21 @@ export const schema = z
       .string()
       .min(1, "ユーザー名は必須です")
       .max(20, "20文字以内で入力してください")
-      .refine((value) =>
-        !/\s/.test(value),"空白は無効です"),
+      .refine((value) => !/\s/.test(value), "空白は無効です"),
     mailAddress: z
       .string()
       .email("正しいメールアドレスを入力してください")
-      .refine((value) =>
-        !/\s/.test(value),"空白は無効です"),
+      .refine((value) => !/\s/.test(value), "空白は無効です"),
     password: z
       .string()
       .min(6, "パスワードは6文字以上で入力してください")
       .max(20, "パスワードは20文字以内で入力してください")
-      .refine((value) =>
-        !/\s/.test(value),"空白は無効です"),
+      .refine((value) => !/\s/.test(value), "空白は無効です"),
     passwordConfirm: z
       .string()
       .min(6, "確認用パスワードは6文字以上で入力してください")
       .max(20, "確認用パスワードは20文字以内で入力してください")
-      .refine((value) =>
-        !/\s/.test(value),"空白は無効です"),
+      .refine((value) => !/\s/.test(value), "空白は無効です"),
   })
   .refine((data) => data.password === data.passwordConfirm, {
     message: "パスワードが一致しません",
