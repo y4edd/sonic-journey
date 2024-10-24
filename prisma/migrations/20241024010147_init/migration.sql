9cd3e7d98611c7c3212f87c4e1aad9a9aead239c
shotaniguchi <sho.taniguchi@rakus-partners.co.jp>
2024-10-24 10:02:52 +0900

    refuctor: song_api_idがIntの上限を超えてしまうためbigintに型を変更

---
+++ prisma/migrations/20241024010147_init/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Favorite_Song" ALTER COLUMN "api_song_id" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "History" ALTER COLUMN "api_song_id" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "Playlist_Song" ALTER COLUMN "api_song_id" SET DATA TYPE BIGINT;
