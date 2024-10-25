1f9929e183df814bcdcdb89a477565c09cac05f1
fukuda0801 <nagisano0801@gmail.com>
2024-10-25 15:27:42 +0900

    feat: Providerでラップし、子孫コンポーネントがコンテキストの値を使用可能にする

--- src/components/music/AlbumSingles/AlbumSingles.tsx
+++ src/components/music/AlbumSingles/AlbumSingles.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import type { AlbumSingle } from "@/types/deezer";
 import AlbumSingleSong from "../AlbumSingleSong/AlbumSingleSong";
 import styles from "./AlbumSingles.module.css";
+import { AlbumAudioProvider } from "@/context/AlbumAudioContext";
 
 type AlbumSong = {
   id: number;
@@ -10,20 +13,23 @@ type AlbumSong = {
 
 const AlbumSingles = ({ singles }: { singles: AlbumSingle[] }) => {
   return (
-    <div className={styles.albumSinglesContent}>
-      {singles.map((song: AlbumSong, index: number) => {
-        return (
-          <div key={song.id} className={styles.albumSingleSong}>
-            <AlbumSingleSong
-              id={song.id}
-              title={song.title}
-              preview={song.preview}
-              num={index + 1}
-            />
-          </div>
-        );
-      })}
-    </div>
+    // Providerでラップすると、子孫コンポーネントがコンテキストにアクセスできるようになる
+    <AlbumAudioProvider>
+      <div className={styles.albumSinglesContent}>
+        {singles.map((song: AlbumSong, index: number) => {
+          return (
+            <div key={song.id} className={styles.albumSingleSong}>
+              <AlbumSingleSong
+                id={song.id}
+                title={song.title}
+                preview={song.preview}
+                num={index + 1}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </AlbumAudioProvider>
   );
 };
 
