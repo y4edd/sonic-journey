f1c06d834cb6ccd633cb46c209543b484c1b7e9b
shotaniguchi <sho.taniguchi@rakus-partners.co.jp>
2024-10-28 09:10:58 +0900

    refuctor: artist_api_idをbigintに変更

--- prisma/migrations/20241023064138_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Pick_Song" ALTER COLUMN "api_song_id" SET DATA TYPE BIGINT;
--- prisma/migrations/20241024010147_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Favorite_Song" ALTER COLUMN "api_song_id" SET DATA TYPE BIGINT;
-
--- AlterTable
-ALTER TABLE "History" ALTER COLUMN "api_song_id" SET DATA TYPE BIGINT;
-
--- AlterTable
-ALTER TABLE "Playlist_Song" ALTER COLUMN "api_song_id" SET DATA TYPE BIGINT;
