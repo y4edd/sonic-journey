c11db84804c8f3b18755a42734252e5e1050599c
y4edd <yaeyuu0116@icloud.com>
2024-10-28 23:08:53 +0900

    chore: cookie、jsonwebtokenのインストール

--- src/app/api/user/login/route.ts
+++ src/app/api/user/login/route.ts
@@ -3,3 +3,5 @@ import bcrypt from "bcrypt";
 import cookie from "cookie";
 import jwt from "jsonwebtoken";
 import type { NextApiRequest, NextApiResponse } from "next";
+
+const SECRET_KEY = process.env.JWT_SECRET_KEY;
