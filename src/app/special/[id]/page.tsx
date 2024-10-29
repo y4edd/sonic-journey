aa6c92306c7b15479a7cc06c5c170295d9f7a2f6
shotaniguchi <sho.taniguchi@rakus-partners.co.jp>
2024-10-29 10:11:55 +0900

    feat: 各特集ページにパンくずリストを適用

--- src/app/special/[id]/page.tsx
+++ src/app/special/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { Special } from "@/components/special/DetailPage/Special/Special";
 import BreadList from "@/components/top/BreadList/BreadList";
 import type { ReadonlyURLSearchParams } from "next/navigation";
+import { getSpecialImage } from "@/components/special/TopPageLink/TopPageLink";
 
 type SongPageProps = {
   params: { id: number };
@@ -9,13 +10,16 @@ type SongPageProps = {
 
 const Page = async ({ params }: SongPageProps) => {
   const { id } = params;
-
+  const specialOverViews = await getSpecialImage();
+  const specialOverView = specialOverViews[id - 1];
   return (
     <>
       <BreadList
         bread={[
           { link: "/", title: "TOP" },
           { link: "/special", title: "特集" },
+
+          { link: `/special/${id}`, title: `${specialOverView.title}` },
         ]}
       />
       <Special id={id} />
