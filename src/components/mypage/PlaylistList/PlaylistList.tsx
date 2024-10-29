cac6039f5c1115fc99221e79dff5e32f612a723e
Kaito1290 <sirobu9963@gmail.com>
2024-10-29 15:08:09 +0900

    feat: PlaylistListコンポーネントを作成

---
+++ src/components/mypage/PlaylistList/PlaylistList.tsx
@@ -0,0 +1,26 @@
+import type { Playlist } from "@/types/deezer";
+import PlaylistItem from "../PlaylistItem/PlaylistItem";
+import styles from "./PlaylistList.module.css";
+
+const PlaylistList = ({ playlists }: { playlists: Playlist[] }) => {
+  return (
+    <div className={styles.playlistListContainer}>
+      {playlists.length === 0 ? (
+        <p className={styles.noPlaylistMessage}>プレイリストは登録されていません</p>
+      ) : (
+        <ul>
+          {playlists.map((playlist: Playlist) => {
+            return (
+              <li key={playlist.id}>
+                <PlaylistItem playlist={playlist} />
+              </li>
+            );
+          })}
+          <div className={styles.horizon} />
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default PlaylistList;
