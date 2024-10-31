e23017684c716b887f914ba05cf22f3796e05f40
kitamura <kanade01705@gmail.com>
2024-10-31 13:02:54 +0900

    fear: 検索結果を種類ごとに表示するように変更

--- src/components/search/SearchKind/SearchKind.tsx
+++ src/components/search/SearchKind/SearchKind.tsx
@@ -1,14 +1,65 @@
+import Link from "next/link";
+import SearchAlbumResult from "../SearchAlbumResult/SearchAlbumResult";
+import SearchResult from "../SearchResult/SearchResult";
 import styles from "./SearchKind.module.css";
-const SearchKind = () => {
+const SearchKind = ({
+  freeWord,
+  searchTotal,
+  kind,
+}: {
+  freeWord: string;
+  searchTotal: string;
+  kind: string;
+}) => {
   return (
     <div className={styles.display}>
-      {/*FIXME: これから機能作ります */}
       <ul className={styles.kind}>
-        <li className={styles.kindList}>シングル</li>
-        <li className={styles.kindList}>アルバム</li>
-        <li className={styles.kindList}>アーティスト</li>
+        {/* クリックすると項目に対応するクエリパラメータを追加 */}
+        <li className={styles.kindList}>
+          <Link
+            href={`/search?q=${freeWord}&n=${searchTotal}&k=all`}
+            className={kind === "all" ? styles.active : ""}
+          >
+            全て
+          </Link>
+        </li>
+        <li className={styles.kindList}>
+          <Link
+            href={`/search?q=${freeWord}&n=${searchTotal}&k=single`}
+            className={kind === "single" ? styles.active : ""}
+          >
+            シングル
+          </Link>
+        </li>
+        <li className={styles.kindList}>
+          <Link
+            href={`/search?q=${freeWord}&n=${searchTotal}&k=album`}
+            className={kind === "album" ? styles.active : ""}
+          >
+            アルバム
+          </Link>
+        </li>
+        <li className={styles.kindList}>
+          <Link
+            href={`/search?q=${freeWord}&n=${searchTotal}&k=artist`}
+            className={kind === "artist" ? styles.active : ""}
+          >
+            アーティスト
+          </Link>
+        </li>
       </ul>
       <hr />
+      {/* クエリパラメータが合うものを表示 */}
+      {kind === "all" && (
+        <div>
+          <SearchResult freeWord={freeWord} url="music" searchTotal={searchTotal} />
+          <SearchAlbumResult freeWord={freeWord} url="album" />
+        </div>
+      )}
+      {kind === "single" && (
+        <SearchResult freeWord={freeWord} url="music" searchTotal={searchTotal} />
+      )}
+      {kind === "album" && <SearchAlbumResult freeWord={freeWord} url="album" />}
     </div>
   );
 };
