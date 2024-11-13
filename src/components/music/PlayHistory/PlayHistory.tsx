31cd436be94c1750c6ed8ed98a2bd7a58d1d746d
fukuda0801 <nagisano0801@gmail.com>
2024-11-13 14:27:27 +0900

    feat: 視聴履歴表示コンポーネント作成

---
+++ src/components/music/PlayHistory/PlayHistory.tsx
@@ -0,0 +1,34 @@
+import { HISTORY } from "@/constants/constant";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./PlayHistory.module.css";
+
+const PlayHistory = () => {
+  // FIXME: 仮の視聴履歴を使用しています。
+  return (
+    <div className={styles.playHistoryGroup}>
+      {HISTORY.length > 0 ? (
+        HISTORY.map(
+          (song: {
+            id: number;
+            image: string;
+            title: string;
+            artist: string;
+          }) => {
+            return (
+              <Link href="/" key={song.id} className={styles.playHistorySong}>
+                <Image src={song.image} alt={`${song.title}の画像`} width={150} height={150} />
+                <p>{song.title}</p>
+                <p>{song.artist}</p>
+              </Link>
+            );
+          },
+        )
+      ) : (
+        <p className={styles.nothingHistory}>視聴履歴がありません</p>
+      )}
+    </div>
+  );
+};
+
+export default PlayHistory;
