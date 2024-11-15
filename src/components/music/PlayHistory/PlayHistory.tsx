22fa4865de4a8250f49bf8d46e2a0281fb80305b
fukuda0801 <nagisano0801@gmail.com>
2024-11-15 14:38:02 +0900

    feat: 楽曲試聴履歴表示コンポーネントにて、ログインユーザーの試聴楽曲情報を取得する処理を作成

--- src/components/music/PlayHistory/PlayHistory.tsx
+++ src/components/music/PlayHistory/PlayHistory.tsx
@@ -1,29 +1,38 @@
 import { HISTORY } from "@/constants/constant";
+import type { DeezerSong } from "@/types/deezer";
+import { getSong } from "@/utils/apiFunc";
+import { getTokenFromCookie } from "@/utils/getTokenFromCookie";
+import { getPlayHistory } from "@/utils/history";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./PlayHistory.module.css";
 
-const PlayHistory = () => {
-  // FIXME: 仮の視聴履歴を使用しています。
+const PlayHistory = async () => {
+  // クッキーからトークン取得
+  const token = getTokenFromCookie();
+  // ログインユーザーの試聴履歴楽曲のidを取得
+  const playHistory = await getPlayHistory(token, 6);
+  // 取得したidを使って楽曲情報を取得
+  const playHistories = await Promise.all(playHistory.songIds.map((id: number) => getSong(id)));
+
+  const playHistorySongs = playHistories.map((playHistorySong) => {
+    return playHistorySong.resSongData;
+  });
+
+  console.log(playHistorySongs);
+
   return (
     <div className={styles.playHistoryGroup}>
-      {HISTORY.length > 0 ? (
-        HISTORY.map(
-          (song: {
-            id: number;
-            image: string;
-            title: string;
-            artist: string;
-          }) => {
-            return (
-              <Link href="/" key={song.id} className={styles.playHistorySong}>
-                <Image src={song.image} alt={`${song.title}の画像`} width={150} height={150} />
-                <p>{song.title}</p>
-                <p>{song.artist}</p>
-              </Link>
-            );
-          },
-        )
+      {playHistorySongs.length > 0 ? (
+        playHistorySongs.map((song: DeezerSong) => {
+          return (
+            <Link href={`/music/${song.id}`} key={song.id} className={styles.playHistorySong}>
+              <Image src={song.cover_xl} alt={`${song.title}の画像`} width={150} height={150} />
+              <p>{song.title}</p>
+              <p>{song.artist.name}</p>
+            </Link>
+          );
+        })
       ) : (
         <p className={styles.nothingHistory}>試聴履歴がありません</p>
       )}
