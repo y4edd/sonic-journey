9a244757ca4c2e4112ccc5e34a765d8fce0e6a2f
Kaito1290 <sirobu9963@gmail.com>
2024-11-15 19:39:48 +0900

    feat: お気に入りアーティストを取得するAPIを作成

--- src/utils/apiFunc.ts
+++ src/utils/apiFunc.ts
@@ -292,3 +292,23 @@ export const checkLoggedInServer = async (token: string): Promise<boolean> => {
     return false;
   }
 };
+
+// DBからお気に入りアーティストのidと更新日を取得する関数（Cookieのtokenを引数にとる）
+export const getFavoriteArtists = async (token: string) => {
+  try {
+    const res = await fetch("http://localhost:3000/api/favoriteArtists", {
+      cache: "no-cache",
+      headers: {
+        Cookie: token,
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error("データが見つかりませんでした");
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
