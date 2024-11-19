b4d50bc4807d2e91a67ae9faef04b7cc10d0b80e
kitamura <kanade01705@gmail.com>
2024-11-19 11:17:50 +0900

    fix: biome忘れ

--- src/app/api/freeArtistSearch/route.ts
+++ src/app/api/freeArtistSearch/route.ts
@@ -6,14 +6,12 @@ export const GET = async (request: NextRequest) => {
   try {
     const { searchParams } = request.nextUrl;
     const artist = searchParams.get("artist");
-    const getArtist = await fetch(
-      `https://api.deezer.com/search/artist?q=${artist}`
-    );
+    const getArtist = await fetch(`https://api.deezer.com/search/artist?q=${artist}`);
 
     if (!getArtist) {
       return NextResponse.json(
         { message: "アーティスト情報が見つかりませんでした" },
-        { status: 404 }
+        { status: 404 },
       );
     }
 
@@ -30,9 +28,6 @@ export const GET = async (request: NextRequest) => {
     return NextResponse.json({ resultData }, { status: 200 });
   } catch (error) {
     console.error(error);
-    return NextResponse.json(
-      { message: "サーバーエラーが発生しました" },
-      { status: 500 }
-    );
+    return NextResponse.json({ message: "サーバーエラーが発生しました" }, { status: 500 });
   }
 };
