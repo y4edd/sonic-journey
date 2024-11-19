31b23ab908ab0f6dd677b0836a6b60cb999f26f3
kitamura <kanade01705@gmail.com>
2024-11-19 11:26:29 +0900

    test: SearchArtistContentのテストを作成

---
+++ src/components/search/SearchArtistResultComtent/SearchArtistContent.test.tsx
@@ -0,0 +1,36 @@
+import type { DeezerArtist } from "@/types/deezer";
+import { render, screen } from "@testing-library/react";
+import SearchArtistResultContent from "./SearchArtistResultContent";
+
+const mockResult: DeezerArtist = {
+  id: 333,
+  name: "test",
+  picture_xl: "https://sample.com/sample-cover.jpg",
+};
+
+describe("SearchArtistContentの単体テスト", () => {
+  test("アーティスト名が正しく表示されているか", () => {
+    render(<SearchArtistResultContent result={mockResult} url="artist" />);
+
+    expect(screen.getByText("test")).toBeInTheDocument();
+  });
+
+  test("画像が正しく表示されているか", () => {
+    render(<SearchArtistResultContent result={mockResult} url="artist" />);
+
+    const image = screen.getByRole("img");
+
+    // srcが正しいか
+    expect(image).toHaveAttribute("src", "https://sample.com/sample-cover.jpg");
+    // altが正しいか
+    expect(image).toHaveAttribute("alt", "testの画像");
+  });
+
+  test("リンクが正しいかどうか", () => {
+    render(<SearchArtistResultContent result={mockResult} url="artist" />);
+
+    const link = screen.getByRole("link");
+
+    expect(link).toHaveAttribute("href", `/artist/${mockResult.id}`);
+  });
+});
