68183842da5ee36c2d037204988fca88671e6b06
y4edd <yaeyuu0116@icloud.com>
2024-11-22 14:06:47 +0900

    fix: APIのディレクトリ整理

---
+++ src/app/api/favoriteArtists/route.ts
@@ -0,0 +1,49 @@
+import prisma from "@/lib/prisma";
+import { getUserIdFromToken } from "@/utils/getUserIdFromToken";
+import { type NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  try {
+    // NOTE: ログインユーザーの確認
+    const token = req.cookies.get("token")?.value;
+    if (!token) {
+      return NextResponse.json({ message: "ログインが必要です" }, { status: 401 });
+    }
+
+    const userId = getUserIdFromToken(token);
+    if (!userId) {
+      return NextResponse.json({ message: "認証に失敗しました" }, { status: 401 });
+    }
+
+    // NOTE: DBからお気に入りアーティストを取得する
+    const favoriteArtists = await prisma.favorite_Artist.findMany({
+      where: {
+        user_id: userId,
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+      select: {
+        api_artist_id: true,
+        updatedAt: true,
+      },
+    });
+
+    // NOTE: お気に入りアーティストが登録されていない場合は空の配列を返す
+    if (!favoriteArtists.length) {
+      return NextResponse.json({ resultData: [] }, { status: 200 });
+    }
+
+    const resultData = favoriteArtists.map((artist) => {
+      return {
+        artistId: Number(artist.api_artist_id),
+        updatedAt: artist.updatedAt,
+      };
+    });
+
+    return NextResponse.json({ resultData }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "サーバーエラーが発生しました" }, { status: 500 });
+  }
+};
---
+++ src/app/api/favorites/postSongs/route.ts
@@ -0,0 +1,40 @@
+import prisma from "@/lib/prisma";
+import { getUserIdFromToken } from "@/utils/getUserIdFromToken";
+import { type NextRequest, NextResponse } from "next/server";
+
+type Body = {
+  musicId: number;
+};
+
+export const POST = async (req: NextRequest) => {
+  try {
+    // NOTE: ログインユーザーのidを取得する
+    const token = req.cookies.get("token")?.value;
+    if (!token) {
+      return NextResponse.json({ message: "ログインが必要です" }, { status: 401 });
+    }
+
+    const userId = getUserIdFromToken(token);
+    if (!userId) {
+      return NextResponse.json({ message: "認証に失敗しました" }, { status: 401 });
+    }
+    const body: Body = await req.json();
+    const musicId = body.musicId;
+    if (!body) {
+      throw new Error("楽曲データの受け渡しに失敗しました");
+    }
+
+    // NOTE: DBにお気に入り楽曲を追加する
+    await prisma.favorite_Song.create({
+      data: {
+        user_id: userId,
+        api_song_id: BigInt(musicId),
+      },
+    });
+
+    return NextResponse.json({ message: "お気に入り楽曲に追加されました" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "サーバーエラーが発生しました" }, { status: 500 });
+  }
+};
