c55227fe159113de0d204933f8a614da66200402
y4edd <yaeyuu0116@icloud.com>
2024-11-22 19:18:14 +0900

    アーティストのお気に入り追加ボタンをコンポーネント化

--- src/components/music/ArtistFavoriteButton/ArtistFavoriteButton.tsx
+++ src/components/music/ArtistFavoriteButton/ArtistFavoriteButton.tsx
@@ -1,7 +1,17 @@
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import styles from "./ArtistFavoriteButton.module.css";
+
 const ArtistFavoriteButton = () => {
   return (
-    <div>ArtistFavoriteButton</div>
+    <>
+      <div className={styles.artistInfoContent}>
+      <button>
+        <FavoriteBorderIcon />
+        お気に入りに追加
+      </button>
+      </div>
+    </>
   )
-}
+};
 
 export default ArtistFavoriteButton;
--- src/components/music/ArtistInfo/ArtistInfo.tsx
+++ src/components/music/ArtistInfo/ArtistInfo.tsx
@@ -1,16 +1,13 @@
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import Image from "next/image";
 import styles from "./ArtistInfo.module.css";
+import ArtistFavoriteButton from "../ArtistFavoriteButton/ArtistFavoriteButton";
 
 const ArtistInfo = ({ image, name }: { image: string; name: string }) => {
   return (
     <div className={styles.artistInfoContent}>
       <Image src={image} alt={`${name}の画像`} width={250} height={250} />
       <p>{name}</p>
-      <div>
-        <FavoriteBorderIcon />
-        <p>お気に入りに追加</p>
-      </div>
+      <ArtistFavoriteButton />
     </div>
   );
 };
