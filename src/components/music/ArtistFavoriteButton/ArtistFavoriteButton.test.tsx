9dadaf69813da7f551f63ddefbeb0cac1cfa2cdb
y4edd <yaeyuu0116@icloud.com>
2024-11-23 17:00:06 +0900

    fix: インデント修正

--- src/components/music/ArtistFavoriteButton/ArtistFavoriteButton.test.tsx
+++ src/components/music/ArtistFavoriteButton/ArtistFavoriteButton.test.tsx
@@ -1,21 +1,21 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import ArtistFavoriteButton from "./ArtistFavoriteButton";
 
-const apiMock =jest.fn();
+const apiMock = jest.fn();
 
 describe("ArtistFavoriteButtonコンポーネントのテスト", () => {
   test("コンポーネントがpropsを受け取り、画面に表示される", () => {
     render(<ArtistFavoriteButton id={88} />);
-    const button = screen.getByRole("button", {name: "お気に入りに追加"});
+    const button = screen.getByRole("button", { name: "お気に入りに追加" });
     expect(button).toBeInTheDocument();
   });
 
   test("非同期処理が完了したとき、アラートが表示される", async () => {
-    apiMock.mockResolvedValue({message: "お気に入りアーティストに追加されました" });
+    apiMock.mockResolvedValue({ message: "お気に入りアーティストに追加されました" });
     window.alert = jest.fn();
     render(<ArtistFavoriteButton id={88} />);
 
-    const button = screen.getByRole("button", {name: "お気に入りに追加"});
+    const button = screen.getByRole("button", { name: "お気に入りに追加" });
 
     fireEvent.click(button);
 
