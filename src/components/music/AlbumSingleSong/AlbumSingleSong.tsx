50a26f1d9c9ea86b954f7c00346e58870d18763e
y4edd <yaeyuu0116@icloud.com>
2024-11-26 17:12:16 +0900

    feat: アルバム内からの楽曲ページにおける登録済み切替表示実装

--- src/components/music/AlbumSingleSong/AlbumSingleSong.tsx
+++ src/components/music/AlbumSingleSong/AlbumSingleSong.tsx
@@ -3,9 +3,12 @@
 import { useAlbumAudio } from "@/context/AlbumAudioContext";
 import { savePlayHistory } from "@/utils/history";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import DoneIcon from '@mui/icons-material/Done';
 import Link from "next/link";
 import AlbumSingleSongAudio from "../AlbumSingleSongAudio/AlbumSingleSongAudio";
 import styles from "./AlbumSingleSong.module.css";
+import { useEffect, useState } from "react";
+import { fetchUser, getFavoriteSongsForFav } from "@/utils/apiFunc";
 
 type AlbumSingleSongProps = {
   id: number;
@@ -14,7 +17,15 @@ type AlbumSingleSongProps = {
   preview: string;
 };
 
+type FavoriteSongs = {
+  resultData :{
+    songId: number,
+    updatedAt: Date
+}[]
+};
+
 const AlbumSingleSong = ({ id, num, title, preview }: AlbumSingleSongProps) => {
+  const [isFav, setIsFav] = useState<boolean>(false);
   // コンテキストからstateを呼び出す
   const { currentlyPlayingId, setCurrentlyPlayingId } = useAlbumAudio();
 
@@ -35,6 +46,23 @@ const AlbumSingleSong = ({ id, num, title, preview }: AlbumSingleSongProps) => {
   // 楽曲をナンバリングするための記述
   const displayNum = num.toString().padStart(2, "0");
 
+    // NOTE: DBから取得したお気に入り楽曲とidを比較し、お気に入りボタンの表示を変える
+    const doneFav = async () => {
+      // NOTE: ログイン状態を確認し、userIdを返す
+      const userId:string = await fetchUser();
+      // NOTE: DBからお気に入り楽曲を取得。
+      const favoriteSongs:FavoriteSongs = await getFavoriteSongsForFav(userId);
+      const songIds = favoriteSongs.resultData.map((song) => song.songId);
+      // NOTE: もしfavoriteSongsのなかのsongIdとidに、一致するものがあればisFavをtrueにする
+      if(songIds.includes(id)) {
+        setIsFav(true);
+      }
+    };
+  
+    useEffect(()=> {
+      doneFav();
+    }, []);
+
   // 楽曲をお気に入り登録
   const postFavorite = async () => {
     try {
@@ -49,10 +77,38 @@ const AlbumSingleSong = ({ id, num, title, preview }: AlbumSingleSongProps) => {
         return;
       }
       alert("お気に入りに登録されました");
+      setIsFav(true);
     } catch (error) {
       console.error(error);
     }
   };
+
+    // お気に入り楽曲削除
+    const deleteFavorite = async () => {
+      try {
+        const response = await fetch("/api/favoriteSongs", {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            songIds: [id],
+          }),
+        });
+        if (!response.ok) {
+          const error = await response.json();
+          console.error(error);
+          alert(error.message);
+          return;
+        }
+  
+        alert("お気に入り楽曲から削除されました");
+        setIsFav(false);
+      } catch (error) {
+        console.error(error);
+        alert("ネットワークエラーです");
+      }
+    };
 
   return (
     <div className={styles.albumSingleContent}>
@@ -68,17 +124,37 @@ const AlbumSingleSong = ({ id, num, title, preview }: AlbumSingleSongProps) => {
             {displayNum}: {title}
           </Link>
         </p>
-        <button type="button" onClick={postFavorite}>
-          <FavoriteIcon
-            sx={{
-              fontSize: 16,
-              color: "#fc9aff",
-              cursor: "pointer",
-            }}
-            role="img"
-            aria-hidden="false"
-          />
-        </button>
+        {isFav ? (
+          <>
+            <button type="button" onClick={deleteFavorite} className={styles.deleteButton}>
+              <DoneIcon
+                sx={{
+                  fontSize: 16,
+                  color: "#a9a9a9",
+                  cursor: "pointer",
+                }}
+                role="img"
+                aria-hidden="false"
+              />
+            </button>
+          </>
+        )
+          : (
+            <>
+              <button type="button" onClick={postFavorite} className={styles.postButton}>
+                <FavoriteIcon
+                  sx={{
+                    fontSize: 16,
+                    color: "#fc9aff",
+                    cursor: "pointer",
+                  }}
+                  role="img"
+                  aria-hidden="false"
+                />
+              </button>
+            </>
+            )
+        }
       </div>
     </div>
   );
