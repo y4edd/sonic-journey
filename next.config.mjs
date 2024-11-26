bbacae2fcc9f7daa4b7c43479e8a6fe9a1e33895
y4edd <yaeyuu0116@icloud.com>
2024-11-26 09:31:40 +0900

    feat: お気に入り押下後のCSS、イベントを追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["e-cdns-images.dzcdn.net"],
+    domains: ["cdn-images.dzcdn.net"],
   },
 };
 
--- src/components/music/FavoriteButton/FavoriteButton.tsx
+++ src/components/music/FavoriteButton/FavoriteButton.tsx
@@ -1,8 +1,16 @@
 "use client";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import DoneIcon from '@mui/icons-material/Done';
 import styles from "./FavoriteButton.module.css";
+import { useState } from "react";
 
 const FavoriteButton = ({ id }: { id: number }) => {
+  const [isFav, setIsFav] = useState<boolean>(false);
+
+  // 初期状態でお気に入り追加済みかどうかを確認し、レンダリング
+  
+
+  // お気に入り楽曲追加
   const postFavorite = async () => {
     try {
       const response = await fetch("/api/favoriteSongs", {
@@ -22,6 +30,34 @@ const FavoriteButton = ({ id }: { id: number }) => {
       }
 
       alert("お気に入り楽曲に追加されました");
+      setIsFav(true);
+    } catch (error) {
+      console.error(error);
+      alert("ネットワークエラーです");
+    }
+  };
+
+  // お気に入り楽曲削除
+  const deleteFavorite = async () => {
+    try {
+      const response = await fetch("/api/favoriteSongs", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          songIds: [id],
+        }),
+      });
+      if (!response.ok) {
+        const error = await response.json();
+        console.error(error);
+        alert(error.message);
+        return;
+      }
+
+      alert("お気に入り楽曲から削除されました");
+      setIsFav(false);
     } catch (error) {
       console.error(error);
       alert("ネットワークエラーです");
@@ -30,10 +66,23 @@ const FavoriteButton = ({ id }: { id: number }) => {
 
   return (
     <>
-      <button type="button" className={styles.songInfoAddFavorite} onClick={postFavorite}>
-        <FavoriteBorderIcon />
-        お気に入りに追加
-      </button>
+      {isFav ? (
+        <>
+          <button type="button" className={styles.songInfoAddedFavorite} onClick={deleteFavorite}>
+            <DoneIcon />
+            お気に入りに追加済み
+          </button>
+        </>
+      )
+        : (
+          <>
+            <button type="button" className={styles.songInfoAddFavorite} onClick={postFavorite}>
+              <FavoriteBorderIcon />
+              お気に入りに追加
+            </button>
+          </>
+        )
+      }
     </>
   );
 };
