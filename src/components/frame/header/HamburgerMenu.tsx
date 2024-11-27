5bf32b913ce604e00dea20c534d1358c93596bb4
shotaniguchi <sho.taniguchi@rakus-partners.co.jp>
2024-11-27 09:24:39 +0900

    feat: ログイン状態によりハンバーガーメニューの表記を変更

--- src/components/frame/header/HamburgerMenu.tsx
+++ src/components/frame/header/HamburgerMenu.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { UseHamburgerOpen } from "@/hooks/header/useHamburgerOpen";
+import { fetchUser } from "@/utils/apiFunc";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import HeadphonesIcon from "@mui/icons-material/Headphones";
 import HistoryIcon from "@mui/icons-material/History";
@@ -12,10 +15,24 @@ import PeopleOutlineIcon from "@mui/icons-material/PeopleOutline";
 import PersonIcon from "@mui/icons-material/Person";
 import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "./HamburgerMenu.module.css";
 
 export const HamburgerMenu = () => {
+  const [user, setUser] = useState<string | null>(null);
   const { openMenu, openMenuClick, hamburgerLink } = UseHamburgerOpen();
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: ハンバーガーメニューの開閉により更新
+  useEffect(() => {
+    const getUser = async () => {
+      const data = await fetchUser();
+      if (data) {
+        setUser(data.id);
+      }
+    };
+    getUser();
+  }, [openMenu]);
+
   return (
     <>
       <div className={styles.hamburgerContainer}>
@@ -95,38 +112,45 @@ export const HamburgerMenu = () => {
                   &nbsp;ユーザー
                 </li>
                 {/* ログイン情報により記載内容変更 */}
-                <li
-                  className={styles.hamburgerinList}
-                  onClick={() => hamburgerLink("/user/login")}
-                  onKeyDown={() => hamburgerLink("/user/login")}
-                >
-                  <LoginIcon fontSize="large" />
-                  &nbsp;ログイン
-                </li>
-                <li
-                  className={styles.hamburgerinList}
-                  onClick={() => hamburgerLink("/user/register")}
-                  onKeyDown={() => hamburgerLink("/user/register")}
-                >
-                  <HowToRegIcon fontSize="large" />
-                  &nbsp;新規登録
-                </li>
-                <li
-                  className={styles.hamburgerinList}
-                  onClick={() => hamburgerLink("/user/logout")}
-                  onKeyDown={() => hamburgerLink("/user/logout")}
-                >
-                  <LogoutIcon fontSize="large" />
-                  &nbsp;ログアウト
-                </li>
-                <li
-                  className={styles.hamburgerinList}
-                  onClick={() => hamburgerLink("/user/[:id]")}
-                  onKeyDown={() => hamburgerLink("/user/[:id]")}
-                >
-                  <AccountBoxIcon fontSize="large" />
-                  &nbsp;アカウント情報
-                </li>
+                {user ? (
+                  <>
+                    <li
+                      className={styles.hamburgerinList}
+                      onClick={() => hamburgerLink("/user/logout")}
+                      onKeyDown={() => hamburgerLink("/user/logout")}
+                    >
+                      <LogoutIcon fontSize="large" />
+                      &nbsp;ログアウト
+                    </li>
+                    <li
+                      className={styles.hamburgerinList}
+                      onClick={() => hamburgerLink("/user/[:id]")}
+                      onKeyDown={() => hamburgerLink("/user/[:id]")}
+                    >
+                      <AccountBoxIcon fontSize="large" />
+                      &nbsp;アカウント情報
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li
+                      className={styles.hamburgerinList}
+                      onClick={() => hamburgerLink("/user/login")}
+                      onKeyDown={() => hamburgerLink("/user/login")}
+                    >
+                      <LoginIcon fontSize="large" />
+                      &nbsp;ログイン
+                    </li>
+                    <li
+                      className={styles.hamburgerinList}
+                      onClick={() => hamburgerLink("/user/register")}
+                      onKeyDown={() => hamburgerLink("/user/register")}
+                    >
+                      <HowToRegIcon fontSize="large" />
+                      &nbsp;新規登録
+                    </li>
+                  </>
+                )}
               </ul>
             </div>
           </>
