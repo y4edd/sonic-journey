21a828169b14315aae97c8b96b006bbce8c64293
Kaito1290 <sirobu9963@gmail.com>
2024-12-02 12:03:38 +0900

    fix: UseGenreNumChange()をクライアントサイドで処理するように変更

--- src/app/genre/page.tsx
+++ src/app/genre/page.tsx
@@ -4,9 +4,14 @@ import GenreState from "@/components/genre/GenreState/GenreState";
 import { ChartTitle } from "@/components/newChart/ChartTitle/ChartTitle";
 import BreadList from "@/components/top/BreadList/BreadList";
 import { UseGenreNumChange } from "@/hooks/chart/useGenreNumChange";
+import { Suspense } from "react";
+
+const GenreContent = () => {
+  const { transitionId } = UseGenreNumChange();
+  return <GenreState transitionId={transitionId} />;
+};
 
 const Page = () => {
-  const { transitionId } = UseGenreNumChange();
   return (
     <>
       <BreadList
@@ -16,7 +21,9 @@ const Page = () => {
         ]}
       />
       <ChartTitle title="ジャンル" />
-      <GenreState transitionId={transitionId} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <GenreContent />
+      </Suspense>
     </>
   );
 };
