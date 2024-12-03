42c55efe1bf416198741aa384f536c5911bb8dae
kitamura <kanade01705@gmail.com>
2024-12-03 11:59:56 +0900

    feat: 削除機能を実装

--- src/components/mypage/DeleteButton/DeleteButton.tsx
+++ src/components/mypage/DeleteButton/DeleteButton.tsx
@@ -1,13 +1,29 @@
+"use client";
+import { deletePlayHistory } from "@/utils/history";
 import DeleteIcon from "@mui/icons-material/Delete";
-import styles from "./Deletebutton.module.css";
+import { useRouter } from "next/navigation";
+import styles from "./DeleteButton.module.css";
 
-const DeleteButton = () => {
+const DeleteButton = ({ userId }: { userId: string }) => {
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    const res = await deletePlayHistory(userId);
+
+    if (res) {
+      router.refresh();
+    } else {
+      alert("再生履歴の削除に失敗しました");
+    }
+  };
   return (
     // FIXME: ボタン押下時に履歴の削除処理を呼び出す
     <div className={styles.container}>
       <div className={styles.deleteButtonWrapper}>
         <DeleteIcon />
-        <button type="button">履歴を削除</button>
+        <button type="button" onClick={handleDelete}>
+          履歴を削除
+        </button>
       </div>
     </div>
   );
